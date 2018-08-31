f7849e13a8ca07356c3e7faa0551787fb62ee066
JennaN23 <jennan@outlook.com>
2018-08-31 08:08:01 -0700

    Added self destruct button.

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.uselessmachine;
 
 import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
+    private ConstraintLayout constraintLayoutBackground;
 
     public static final String TAG = MainActivity.class.getSimpleName();
     @Override
@@ -25,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners() {
         // TODO self destruct button
+        buttonSelfDestruct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSelfDestructSequence();
+
+            }
+        });
+
         switchUseless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -37,6 +48,32 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void startSelfDestructSequence() {
+        //disable the button
+        buttonSelfDestruct.setEnabled(false);
+        //start a 10 second countdown timer that updates the display every second
+        startCountDownTimer();
+        //want to display
+        //destruct in 10...
+        //destruct in 9...
+        //at the end, close the activity
+        //call the finish() method
+
+    }
+
+    private void startCountDownTimer() {
+        new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Toast.makeText(MainActivity.this, "Destruct in " + millisUntilFinished/1000 + "...",
+                        Toast.LENGTH_SHORT).show();
+            }
+            public void onFinish() {
+                finish();
+            }
+        }.start();
     }
 
     private void startSwitchOffTimer() {
@@ -61,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
     private void wireWidgets() {
         buttonSelfDestruct = findViewById(R.id.button_main_selfdestruct);
         switchUseless = findViewById(R.id.switch_main_useless);
+        constraintLayoutBackground = findViewById(R.id.activity_main_background);
     }
 
 
